11280391851cf77c74f448aa8f8dba6af52c02c5
Simon Sahlin <simon.sahlin@utb.ecutbildning.se>
2021-04-15 13:27:50 +0200

    update to shipmentservice_test

--- shipmentservice/shipmentservice_test.go
+++ shipmentservice/shipmentservice_test.go
@@ -5,8 +5,10 @@ import (
 )
 
 func TestCalculatePriceFromWeight (t *testing.T) {
-    expectation  := CalculatePriceFromWeight(25) //Change to match question asked.
-    actual := CalculatePriceFromWeight(11)//Change to match question asked
+    /*Description of CalculatePriceFromWeight:
+    if actual: 0-10 then exp = 100, if actual: 11-25 then exp = 300, if actual: 26-50 then exp = 500, if actual: 51-1000 then exp = 2000*/
+    expectation  := 2000.0 //price output, use float value to check output
+    actual := CalculatePriceFromWeight(51)//Change to match question asked
     if actual != expectation {
         t.Error("Something went wrong")
     }
